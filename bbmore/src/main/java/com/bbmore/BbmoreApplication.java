3048446fe6b38106e52a02c511b76645bbdc10f8
ehdgus1162 <at1at2at3at4at5@naver.com>
2025-03-20 11:13:02 +0900

    d

---
+++ bbmore/src/main/java/com/bbmore/BbmoreApplication.java
@@ -0,0 +1,13 @@
+package com.bbmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbmoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbmoreApplication.class, args);
+    }
+
+}
---
+++ bbmore/src/test/java/bbmore/BbmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package bbmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbmoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
